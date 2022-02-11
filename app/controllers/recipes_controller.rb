08409f6f796040d6a2ffdbd6d3d12c3b7632c643
Shrikant Limaye <shrikant.limaye@ritikaawood.com>
2022-02-11 18:04:37 +0530

    Model added

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,8 @@
 class RecipesController < ApplicationController
-    before_action :find_recipe
-    def index
+    before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
+    def index
+        @recipes = Recipe.all.order(created_at: 'DESC')
     end
 
     def show
@@ -9,14 +10,25 @@ class RecipesController < ApplicationController
     end
 
     def new
-
+        @recipe = Recipe.new
     end
 
     def create
+        @recipe = Recipe.new(recipe_params)
 
+        if @recipe.save
+            redirect_to @recipe, notice: "Successfully created the recipe!!"
+        else
+            render 'new', notice: "Recipe Not Saved."
+        end
     end
 
     private
+    
+    def recipe_params
+        params.require(:recipe).permit(:title, :description)
+    end
+
     def find_recipe
         @recipe = Recipe.find(params[:id])
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_105320) do
+ActiveRecord::Schema.define(version: 2022_02_11_105702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "recipes", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "instruction", null: false
-    t.text "ingredients", null: false
-    t.string "prep_time"
-    t.text "health_count"
-    t.text "notes"
+    t.string "title"
+    t.text "description"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
